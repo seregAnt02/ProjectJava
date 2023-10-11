a85f9ce1f6fb3aa27542f80e6a555885ed94d01d
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-10-11 13:26:23 +0500

    Create TableModel.java

---
+++ OOP/task_5/models/TableModel.java
@@ -0,0 +1,70 @@
+package OOPDmz.task_5.models;
+
+//import ru.geekbrains.lesson5.presenters.Model;
+import OOPDmz.task_5.presenters.Model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+public class TableModel implements Model {
+
+    private Collection<Table> tables;
+
+
+    /**
+     * Получение списка всех столиков
+     */
+    @Override
+    public Collection<Table> loadTables() {
+
+        if (tables == null){
+            tables = new ArrayList<>();
+
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+        }
+
+        return tables;
+    }
+
+    /**
+     * Бронирование столика
+     * @param reservationDate Дата бронирования
+     * @param name Имя
+     */
+    @Override
+    public int reservationTable(Date reservationDate, int tableNo, String name) {
+        for (Table table: loadTables()) {
+            if (table.getNo() == tableNo){
+                Reservation reservation = new Reservation(reservationDate, name);
+                table.getReservations().add(reservation);
+                return reservation.getId();
+            }
+        }
+        throw  new RuntimeException("Некорректный номер столика.");
+    }
+
+    /**     
+     * Отменить бронирование по номеру
+     * @param oldReservation старый номер брони
+     * @param reservationDate дата бронирования
+     * @param tableNo номер столика
+     * @param name имя     
+     */
+    public int changeReservationTable(int oldReservation, Date reservationDate, int tableNo, String name){
+        for(Table table: loadTables()){
+            if(table.getNo() == tableNo){                                                
+                //удаление старого резерва.
+                table.getReservations().clear();                                                
+
+                return oldReservation;            
+            }
+        }
+        throw  new RuntimeException("Некорректный номер столика.");
+    }
+
+}
