b69d1c324fb7aadd5f0696ff59fbf5ac233a1463
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-10-11 13:23:20 +0500

    Create Reservation.java

---
+++ OOP/task_5/models/Reservation.java
@@ -0,0 +1,27 @@
+package OOPDmz.task_5.models;
+
+import java.util.Date;
+
+public class Reservation {
+
+    private static int counter = 1000;
+
+    private final int id;
+
+    protected int getId() {
+        return id;
+    }
+
+    private Date date;
+
+    private String name;
+
+    {
+        id = ++counter;
+    }
+
+    public Reservation(Date date, String name) {
+        this.date = date;
+        this.name = name;
+    }
+}
