7f06c9b7466258e851418c04e0751191127eb9de
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-10-11 13:24:27 +0500

    Create Table.java

---
+++ OOP/task_5/models/Table.java
@@ -0,0 +1,41 @@
+package OOPDmz.task_5.models;
+
+import OOPDmz.task_5.presenters.Model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Locale;
+
+public class Table {
+
+
+    private static int counter;
+
+    private final int no;
+
+    private final Collection<Reservation> reservations = new ArrayList<>();
+
+    /**
+     * Номер столика.
+     * @return 
+     * return int.
+     */
+    public int getNo() {
+        return no;
+    }
+
+    public Collection<Reservation> getReservations() {
+        return reservations;
+    }
+    
+
+    {
+        no = ++counter;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.getDefault(), "Столик #%d", no);
+    }
+
+}
