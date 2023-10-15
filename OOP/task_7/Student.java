413d432c781189e8b4c6e21602203b57b920e608
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-10-15 19:58:32 +0500

    Create Student.java

---
+++ OOP/task_7/Student.java
@@ -0,0 +1,31 @@
+package OOP.task_7.lesson7.observer;
+
+
+/**
+ * model Student.
+ */
+public class Student implements Observer{
+
+    private String name;    
+    private int salary = 2000;
+
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void receiveOffer(String nameCompany, int salary) {
+        if (this.salary <= salary){
+            System.out.printf("Студент %s: Мне нужна эта работа! (компания: %s; заработная плата: %d)\n",
+                    name, nameCompany, salary);            
+            this.salary = salary;            
+        }
+        else {
+            System.out.printf("Студент %s: Я найду работу получше! (компания: %s; заработная плата: %d)\n",
+                    name, nameCompany, salary);
+        }
+    }
+
+
+}
