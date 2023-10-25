fdbf87fc8c694fcf34832f85c840f34b8ef5bb6e
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-10-25 14:39:23 +0500

    Create Answer.java

---
+++ Exception/HW3/Answer.java
@@ -0,0 +1,29 @@
+package Exception.HW3;
+
+import java.util.Arrays;
+
+class Answer {
+    public int[] divArrays(int[] a, int[] b){
+      // Введите свое решение ниже
+            if(a != null && b != null && a.length == b.length){                
+                int[] arr = new int[a.length];
+                for(int i = 0; i < a.length; i++){
+                    if(a[i] > 0 && b[i] > 0){            
+                        arr[i] = a[i] > b[i] ? a[i] / b[i] : b[i] / a[i];
+                    }
+                    if(a[i] < 0 && b[i] < 0){        
+                        arr[i] = a[i] < b[i] ? a[i] / b[i] : b[i] / a[i];                   
+                    }        
+                    if(a[i] > 0 && b[i] < 0){
+                        arr[i] = a[i] / b[i];
+                    }
+                    if(a[i] < 0 && b[i] > 0){
+                        arr[i] = a[i] / b[i];
+                    }
+                }
+                return arr;
+            }      
+        return new int[]{ 0 };        
+    }
+
+}
