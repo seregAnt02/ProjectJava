c394302dddeb21f893d60afd018e5ba14ae2f235
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-10-25 14:34:53 +0500

    Create Answer.java

---
+++ Exception/HW1/Answer.java
@@ -0,0 +1,20 @@
+package Exception.HW1;
+
+class Answer {
+    public void arrayOutOfBoundsException() {
+        // Напишите свое решение ниже
+      int[] arr = new int[]{1, 2, 3};
+      arr[3] = 3;
+}
+
+    public void divisionByZero() {
+        // Напишите свое решение ниже
+        int number = 10 / 0;
+      
+}
+
+    public void numberFormatException() {
+        // Напишите свое решение ниже
+       Integer number = Integer.parseInt("12x");
+ }
+}
