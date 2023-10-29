bcc8e4025890c56a029313c3f0cd326562bf8537
seregAnt02 <129961993+seregAnt02@users.noreply.github.com>
2023-10-29 12:11:44 +0500

    Create Prize.java

---
+++ ToyShop/model/Prize.java
@@ -0,0 +1,16 @@
+package ToyShop.model;
+
+public class Prize extends Toy {
+
+
+    public Prize(int id, String name) {
+        super(id, name);
+
+    }
+
+    @Override
+    public String toString() {
+        return "\nНомер: " + getId() + "," + " Название: " + "\"" + getName() + "\"";
+    }
+
+}
